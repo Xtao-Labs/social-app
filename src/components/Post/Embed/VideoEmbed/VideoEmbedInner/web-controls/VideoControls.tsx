a55c5de95e0a9fc604bc9c25be7434c90e7f2b56
omg-xtao <100690902+omg-xtao@users.noreply.github.com>
2024-10-25 08:22:01 +0000

    feat: video resolution choose

--- src/components/Post/Embed/VideoEmbed/VideoEmbedInner/web-controls/VideoControls.tsx
+++ src/components/Post/Embed/VideoEmbed/VideoEmbedInner/web-controls/VideoControls.tsx
@@ -284,6 +284,13 @@ export function Controls({
     [onEndHover],
   )
 
+  const onChangeBitrate = (event: ChangeEvent<HTMLSelectElement>) => {
+    if (!hlsRef.current) {
+      return
+    }
+    hlsRef.current.currentLevel = parseInt(event.target.value)
+  }
+
   const showControls =
     ((focused || autoplayDisabled) && !playing) ||
     (interactingViaKeypress ? hasFocus : hovered)
@@ -380,6 +387,15 @@ export function Controls({
             ]}>
             {formatTime(currentTime)} / {formatTime(duration)}
           </Text>
+          {hlsRef.current && (
+            <select onChange={onChangeBitrate}>
+              {hlsRef.current.levels.map((level, id) => (
+                <option key={id} value={id}>
+                  <Text>{level.height}p</Text>
+                </option>
+              ))}
+            </select>
+          )}
           {hasSubtitleTrack && (
             <ControlButton
               active={subtitlesEnabled}
